4f8a60d081ce7e01264d60bf09f93b37e7f2afa5
Anton Batyaev <a.batyaev@gmail.com>
2013-05-27 21:59:20 +0400

    #45 bugfix

--- trunk/ContactList/driverManager.cpp
+++ trunk/ContactList/driverManager.cpp
@@ -8,6 +8,13 @@ const QString XML(".xml");
 const QString SQLITE(".sqlite");
 }
 
+namespace {
+#define ERROR QObject::trUtf8("Error")
+#define ERR_INCORRECT QObject::trUtf8("This file has incorrect format!")
+#define ERR_INCORRECT_SAVE_AS QObject::trUtf8("This file has incorrect format! File will be save as %1")
+#define ERROR_MESSAGE_INCORRECT QMessageBox::warning(new QWidget(), ERROR, ERR_INCORRECT)
+}
+
 DriverManager::DriverManager() :
     m_xml(new DriverXml()),
     m_sqlite(new DriverSqlite())
@@ -21,18 +28,11 @@ DriverManager::~DriverManager()
 bool DriverManager::saveData(const Data::Contacts &data, const QString &path)
 {
     if(path.endsWith(File::XML))
-    {
         return m_xml->saveData(data, path);
-    }
     else if(path.endsWith(File::SQLITE))
-    {
         return m_sqlite->saveData(data, path);
-    }
     else
-    {
-        //FIXME msgbox incorrect format, save as sqlite
         return m_sqlite->saveData(data, path + File::SQLITE);
-    }
 }
 
 Data::Contacts *DriverManager::loadData(const QString &path)
@@ -47,7 +47,7 @@ Data::Contacts *DriverManager::loadData(const QString &path)
     }
     else
     {
-        //FIXME msgbox incorrect format
+        ERROR_MESSAGE_INCORRECT;
         return 0;
     }
 }
@@ -55,14 +55,9 @@ Data::Contacts *DriverManager::loadData(const QString &path)
 bool DriverManager::saveContact(const Data::ContactData &data, const QString &path)
 {
     if(path.endsWith(File::XML))
-    {
         return m_xml->saveContact(data, path);
-    }
     else
-    {
-        //FIXME msgbox incorrect format, save as xml
         return m_xml->saveContact(data, path + File::XML);
-    }
 }
 
 Data::ContactData *DriverManager::loadContact(const QString &path)
@@ -73,7 +68,7 @@ Data::ContactData *DriverManager::loadContact(const QString &path)
     }
     else
     {
-        //FIXME msgbox incorrect format
+        ERROR_MESSAGE_INCORRECT;
         return 0;
     }
 }
