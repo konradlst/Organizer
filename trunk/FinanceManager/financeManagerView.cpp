266c0fee65191bd6da8e8ba4c9dc5151c491451f
Anton Batyaev <a.batyaev@gmail.com>
2013-06-01 23:26:42 +0400

    #50 edit FM

--- trunk/FinanceManager/financeManagerView.cpp
+++ trunk/FinanceManager/financeManagerView.cpp
@@ -6,6 +6,16 @@ FinanceManagerView::FinanceManagerView(QWidget *parent) :
     ui(new Ui::FinanceManagerView)
 {
     ui->setupUi(this);
+    centralWidget()->setLayout(ui->mainLayout);
+    ui->tab_3->setLayout(ui->vlayPlanning);
+    ui->tab->setLayout(ui->vLayStatistic);
+    ui->gbAccounts->setLayout(ui->fLayAccounts);
+    ui->gbCurrentMounth->setLayout(ui->fLayCurrentMounth);
+    ui->gbToday->setLayout(ui->fLayToday);
+
+
+    ui->deCurrentDate->setDate(QDate::currentDate());
+    ui->deCurrentDayOfWeek->setDate(QDate::currentDate());
 }
 
 FinanceManagerView::~FinanceManagerView()
