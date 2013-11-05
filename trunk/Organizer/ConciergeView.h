4f7644170042d06df427cfc092451d050a2ac55b
Anton Batyaev <a.batyaev@gmail.com>
2013-11-05 20:35:15 +0400

    #107 change ConciergeView, delete Calendar project

--- trunk/Organizer/ConciergeView.h
+++ trunk/Organizer/ConciergeView.h
@@ -15,11 +15,15 @@ public:
     explicit ConciergeView(QWidget *parent = 0);
     ~ConciergeView();
 
+private slots:
+    void setToday();
+
 private:
     Ui::ConciergeView *ui;
 
 private:
     void createInterface();
+    void initTimeList(QStringList &list);
 };
 
 #endif // CONCIERGEVIEW2_H
